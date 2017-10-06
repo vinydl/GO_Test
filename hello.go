a24ebe49112093764adf52dc42e80f9ce6db4ce5
vinydl <30833673+vinydl@users.noreply.github.com>
2017-10-06 14:57:28 +0530

    Updated for local environement

--- hello.go
+++ hello.go
@@ -1,14 +1,15 @@
-package hello
+package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
 )
 
-func init() {
-    http.HandleFunc("/", handler)
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprint(w, "Hello  world!")
+	fmt.Fprint(w, "Hello  world!")
 }
